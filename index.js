5c3f8a24191a14e25cd2c3446531c5d727b250e8
marekbran <hermann.adam03@gmail.com>
2024-07-02 17:03:02 +0300

    aaa

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const { exec } = require('child_process');
 const path = require('path');
-const express = require('express');
-const app = express();
 
 // Change the working directory to the backend directory
 const backendDir = path.join(__dirname, 'phonebook_backend');
 
 // Command to run the backend server with npm
 const command = 'npm run dev';
-;
 
 exec(command, { cwd: backendDir }, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error starting the backend server: ${error.message}`);
-        return;
-    }
+  if (error) {
+    console.error(`Error starting the backend server: ${error.message}`);
+    return;
+  }
 
-    if (stderr) {
-        console.error(`Backend server stderr: ${stderr}`);
-        return;
-    }
+  if (stderr) {
+    console.error(`Backend server stderr: ${stderr}`);
+    return;
+  }
 
-    console.log(`Backend server stdout: ${stdout}`);
-});
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-  });
-  
-const PORT = 5317;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Backend server stdout: ${stdout}`);
 });
