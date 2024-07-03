2b1fe4b2842d20e6da1d2df00a95c95e010a142e
marekbran <hermann.adam03@gmail.com>
2024-07-04 01:02:19 +0300

    this shit is difficult

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const app = express();
-const mongoose = require('mongoose')
-require('dotenv').config() 
-const Person = require('./models/person')
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Person = require('./models/person');
 
+const app = express();
 
 app.use(express.json());
 app.use(express.static('dist'));
 
 app.use(cors({
-  origin: '*', 
+  origin: '*',
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Access-Control-Allow-Private-Network'],
   preflightContinue: true,
   optionsSuccessStatus: 200
 }));
-
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Private-Network', '*');
@@ -31,99 +29,93 @@ app.use((req, res, next) => {
   }
 });
 
-
 morgan.token('params', (req) => JSON.stringify(req.params));
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :params'));
 
-let persons = [
-  { "id": 1, "name": "Arto Hellas", "number": "040-123456" },
-  { "id": 2, "name": "Ada Lovelace", "number": "39-44-5323523" },
-  { "id": 3, "name": "Dan Abramov", "number": "12-43-234345" },
-  { "id": 4, "name": "Mary Poppendieck", "number": "39-23-6423122" }
-];
-
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-  .then(response => {
-    console.log(`connected successfully to MongoDB`)
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
   })
-  .catch((error) => console.log(error.message + ' did not connect.'));
-
-Person.find({}).then(persons => {
-  persons = persons.map(person => person.toJSON())
-})
-  .catch(error => console.log(error));
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
 
 app.get('/persons', (req, res) => {
-  console.log(persons)
-  console.log(Person)
   Person.find({}).then(persons => {
-    res.json(persons)
+    res.json(persons);
   })
   .catch(error => {
     console.log(error);
-    res.status(500).json({error: 'Error fetching data from database.'})
-  })
-});
-
-app.get('/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.get('/info', (req, res) => {
-  const x = persons.length;
-  const currentTime = new Date().toLocaleString();
-  res.send(`<p>Request received at ${currentTime}, ${x} entries in the phonebook</p>`);
-});
-
-app.delete('/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
-  res.status(200).end();
-});
-
-app.post('/persons/:name/:number', (req, res) => {
-  const body = req.params;
-  const id = Math.random(1000);
-
-  if (!body.name) {
-    return res.status(400).json({ error: 'name missing' });
-  }
-
-  if (!body.number) {
-    return res.status(400).json({ error: 'number missing' });
-  }
-
-  if (persons.find(person => person.name === body.name)) {
-    return res.status(400).json({ error: 'name must be unique' });
-  }
-
-  if (persons.find(person => person.number === body.number)) {
-    return res.status(400).json({ error: 'number must be unique' });
-  }
-
-  const person = {
-    id,
-    name: body.name,
-    number: body.number
-  };
-  person.save().then(savedPerson => {
-    res.json(savedPerson);
+    res.status(500).json({ error: 'Error fetching data from database.' });
   });
 });
 
-const PORT = process.env.PORT
+app.get('/persons/:id', (req, res) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Error fetching data from database.' });
+    });
+});
+
+app.get('/info', (req, res) => {
+  Person.countDocuments({})
+    .then(count => {
+      const currentTime = new Date().toLocaleString();
+      res.send(`<p>Request received at ${currentTime}, ${count} entries in the phonebook</p>`);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Error fetching data from database.' });
+    });
+});
+
+app.delete('/persons/:id', (req, res) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Error deleting data from database.' });
+    });
+});
+
+app.post('/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number missing' });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  });
+
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: 'Error saving data to database.' });
+    });
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
