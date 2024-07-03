6877be38f97c0b7352c8aea324d806599a42a81f
marekbran <hermann.adam03@gmail.com>
2024-07-04 00:09:07 +0300

    actual persons

--- index.js
+++ index.js
@@ -34,7 +34,14 @@ app.use((req, res, next) => {
 
 morgan.token('params', (req) => JSON.stringify(req.params));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :params'));
-let persons = [];
+
+let persons = [
+  { "id": 1, "name": "Arto Hellas", "number": "040-123456" },
+  { "id": 2, "name": "Ada Lovelace", "number": "39-44-5323523" },
+  { "id": 3, "name": "Dan Abramov", "number": "12-43-234345" },
+  { "id": 4, "name": "Mary Poppendieck", "number": "39-23-6423122" }
+];
+
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(response => {
