ac9e86a22200ded7fb3ba6c8992be32f0edb5a4a
marekbran <hermann.adam03@gmail.com>
2024-07-04 03:29:58 +0300

    Edward

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.delete('/persons/:id', (req, res) => {
       res.status(500).json({ error: 'Error deleting data from database.' });
     })
 })
-app.post('/persons', (request, response) => {
+app.post('/persons/:number', (request, response) => {
   console.log('Request Body:', request.body)
   const body = request.body
 
@@ -95,7 +95,7 @@ app.post('/persons', (request, response) => {
   }
 
   const person = new Person({
-    name: body.name,
+    name: "Edward",
     number: body.number
   });
 
