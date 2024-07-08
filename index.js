71f16707b6842fc4653fe0f78f99521fe6738aae
marekbran <hermann.adam03@gmail.com>
2024-07-08 17:35:35 +0300

    eslint approved code

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const Person = require('./models/person');
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(express.static('dist'));
+app.use(express.json())
+app.use(express.static('dist'))
 
 app.use(cors({
   origin: '*',
@@ -19,13 +18,13 @@ app.use(cors({
 }))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Private-Network', '*');
+  res.header('Access-Control-Allow-Private-Network', '*')
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(200);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.sendStatus(200)
   } else {
-    next();
+    next()
   }
 })
 
@@ -44,28 +43,27 @@ const errorHandler = (error, request, response, next) => {
 app.use(errorHandler)
 
 
-morgan.token('params', (req) => JSON.stringify(req.params));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :params'));
+morgan.token('params', (req) => JSON.stringify(req.params))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :params'))
 
 
-app.get('/', (req, res, next) => {
-  res.send('<h1>Hello World!</h1>');
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
-app.get('/api/persons', (req, res, next) => {
-  Person.find({}).then(persons => {
-    res.json(persons);
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(notes => {
+    response.json(notes)
   })
-  .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
     .catch(error => next(error))
@@ -74,8 +72,8 @@ app.get('/api/persons/:id', (req, res, next) => {
 app.get('/info', (req, res, next) => {
   Person.countDocuments({})
     .then(count => {
-      const currentTime = new Date().toLocaleString();
-      res.send(`<p>Request received at ${currentTime}, ${count} entries in the phonebook</p>`);
+      const currentTime = new Date().toLocaleString()
+      res.send(`<p>Request received at ${currentTime}, ${count} entries in the phonebook</p>`)
     })
     .catch(error => next(error))
 })
@@ -83,17 +81,15 @@ app.get('/info', (req, res, next) => {
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findOneAndDelete({ _id: req.params.id })
     .then(() => {
-      res.status(200).end();
+      res.status(200).end()
     })
     .catch(error => next(error))
 })
 app.post('/api/persons', (request, response, next) => {
-  console.log("r", request.body)
   const body = request.body
- 
   if (!body.name || !body.number) {
-    console.log('Missing name or number');
-    return response.status(400).json({ error: 'name or number missing' });
+    console.log('Missing name or number')
+    return response.status(400).json({ error: 'name or number missing' })
   }
 
 
@@ -105,7 +101,7 @@ app.post('/api/persons', (request, response, next) => {
 
   person.save()
     .then(savedPerson => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
     .catch(error => next(error))
 })
@@ -126,7 +122,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 
 mongoose.connect(url)
   .then(() => {
@@ -12,41 +12,41 @@ mongoose.connect(url)
   })
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
-  });
+  })
 
-  const personSchema = new mongoose.Schema({
-    name: { type: String,
+const personSchema = new mongoose.Schema({
+  name: { type: String,
     minLength: 3,
     required: true,
-    },
-    number: {
-      type: String,
-      validate: {
-        validator: function(value) {
-          const parts = value.split('-');
-          if (parts.length !== 2) {
-            return false;
-          }
-          const [firstPart, secondPart] = parts;
-          if (firstPart.length < 2 || firstPart.length > 4) {
-            return false;
-          }
-          if (secondPart.length < 6) {
-            return false;
-          }
-          return /^[0-9]+$/.test(firstPart) && /^[0-9]+$/.test(secondPart)
-        },
-        message: 'Invalid phone number format',
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(value) {
+        const parts = value.split('-')
+        if (parts.length !== 2) {
+          return false
+        }
+        const [firstPart, secondPart] = parts
+        if (firstPart.length < 2 || firstPart.length > 4) {
+          return false
+        }
+        if (secondPart.length < 6) {
+          return false
+        }
+        return /^[0-9]+$/.test(firstPart) && /^[0-9]+$/.test(secondPart)
       },
+      message: 'Invalid phone number format',
     },
-  })
+  },
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-});
+})
 
 module.exports = mongoose.model('Person', personSchema)
